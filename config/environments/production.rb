d7ee49b9f87bad166082fb7ea58083c5ada40c87
pineapple-thief <gluhov1985@gmail.com>
2015-01-19 00:14:32 +1000

    Allright, no errors when rendering mail template, but no mail received either. Postfix seems to think mail is delivered. Maybe this will work?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,7 @@ Brimir::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
 
   # Enable threaded mode
   # config.threadsafe!
